31387bfb46f57a608a0a621d081064c2f1e2fa95
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-07-27 17:58:07 +0100

    Adds search by tag function

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
-
-ENV['RACK_ENV'] = 'development'
 
 
 class BookmarkManager < Sinatra::Base
@@ -22,6 +22,13 @@ class BookmarkManager < Sinatra::Base
   get '/links/new' do
     erb :'links/new'
   end
+
+  get'/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
 
 
   # start the server if ruby file executed directly
