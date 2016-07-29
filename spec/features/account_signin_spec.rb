85715b5110c210288db015081075ab929d90d363
Rylan Gooch <rylangooch@gmail.com>
2016-07-29 10:50:33 +0100

    Started challenge 20

--- spec/features/account_signin_spec.rb
+++ spec/features/account_signin_spec.rb
@@ -9,4 +9,10 @@ feature 'users can signin' do
     expect(User.first.email).to eq('TC@makers.academy.com')
     expect(page).to have_content('Welcome, TC@makers.academy.com')
   end
+
+  scenario 'confirmation password does not match password' do
+    bad_sign_up
+    expect(page).to have_content('Password and confirmation password do not match')
+    expect(route).to eq('/users')
+  end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,3 +6,12 @@ def sign_up(email: 'TC@makers.academy.com', password: 'kiwi', password_confirmat
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
 end
+
+def bad_sign_up(email: 'TC@makers.academy.com', password: 'kiwi', password_confirmation: 'apple')
+    visit '/user/new'
+    expect(page.status_code).to eq(200)
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+end
